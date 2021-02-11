9e9d69b56cef86c95cec32311d148b24da401e93
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2021-02-10 21:19:09 -0500

    Implimented the uMVP and uColor uniforms into the vs and fs, respectively.

--- animal3D SDK/resource/glsl/4x/fs/drawColorUnif_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/drawColorUnif_fs4x.glsl
@@ -24,15 +24,16 @@
 
 #version 450
 
-// ****TO-DO: 
+// ****DONE: 
 //	-> declare color uniform
 //		(hint: correct name is used in codebase)
 //	-> assign uniform directly to output
+
+uniform vec4 uColor;
 
 layout (location = 0) out vec4 rtFragColor;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE RED
-	rtFragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	rtFragColor = uColor;
 }
