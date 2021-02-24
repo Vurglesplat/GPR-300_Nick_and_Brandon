9d080c210dd5d9699146e9d0808469abfe8b5dd9
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2021-02-24 12:59:29 -0500

    Copied our phong fragment shader code to the phong shadow fragment shader

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/drawPhong_shadow_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/drawPhong_shadow_fs4x.glsl
@@ -33,12 +33,39 @@
 //	-> perform manual "perspective divide" on shadow coordinate
 //	-> perform "shadow test" (explained in class)
 
+uniform vec4 uLightPos; 
+uniform vec4 uLightColor;
+uniform float uLightInvRadiusSqr;
+
+uniform sampler2D uTex_dm;
+uniform vec4 uColor; 
+
+
 layout (location = 0) out vec4 rtFragColor;
 
-uniform int uCount;
+in vec4 vNormal;
+in vec4 vPosition;
+in vec2 vTexCoord; 
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE MAGENTA
-	rtFragColor = vec4(1.0, 0.0, 1.0, 1.0);
+	vec4 tex = texture(uTex_dm, vTexCoord); 
+
+	vec4 N = normalize(vNormal);
+	vec4 L = uLightPos - vPosition;
+	float lightDistance = length(L);
+	L = L/lightDistance; //This normalizes L WITHOUT using the normalize function
+						 // which would have called length() again, and we are using it later for attenuation
+
+	float lmbCoeff = max(0.0,dot(N, L));
+	float attenuation = mix(1.0,0.0,lightDistance * uLightInvRadiusSqr); // light intensity is based on distance relative to radius
+	
+	//Determining view and reflection vectors;
+	//thence the phong coefficient
+	vec4 V = normalize(-vPosition);
+	vec4 R = reflect(-L,N);
+	float phongCoeff = max(0.0,dot(R, V));
+
+	vec4 result = tex * uColor * uLightColor * (lmbCoeff + phongCoeff) * attenuation;
+	rtFragColor = vec4(result.rgb,1.0); 
 }
