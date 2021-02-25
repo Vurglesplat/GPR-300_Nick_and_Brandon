3860aec260c5cffc46407ad0351ed88bf5ab1c1d
Brandon Boras <brandon.boras@mymail.champlain.edu>
2021-02-25 04:07:52 -0500

    implementing the blend shader

--- animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBlend_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/01-pipeline/postBlend_fs4x.glsl
@@ -34,10 +34,18 @@ layout (location = 0) out vec4 rtFragColor;
 in vec4 vTexcoord_atlas;
 uniform sampler2D uTex_dm;
 
+uniform sampler2D uImage00, uImage01, uImage02, uImage03;
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE PURPLE
-//	rtFragColor = vec4(0.5, 1.0, 1.0, 1.0);
-	rtFragColor = texture(uTex_dm, vTexcoord_atlas.xy);
+	// this used Dan Buckstein's Lecture 5 on bloom as a basis, specifically the algorithm: screen(A,B,C,D) = 1 - (1-A)(1-B)(1-C)(1-D)
+
+	// inverting each texture, and then multiplying them (inverted so that the dark values intensify each other)
+	vec4 color  = vec4(1.0) - texture(uImage00, vTexcoord_atlas.xy);
+		 color *= vec4(1.0) - texture(uImage00, vTexcoord_atlas.xy);
+		 color *= vec4(1.0) - texture(uImage02, vTexcoord_atlas.xy);
+		 color *= vec4(1.0) - texture(uImage03, vTexcoord_atlas.xy);
+
+	color = vec4(1.0) - color;
+	rtFragColor = color;
 }
