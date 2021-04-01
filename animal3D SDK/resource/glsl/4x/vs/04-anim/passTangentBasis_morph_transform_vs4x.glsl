494ed51680561869582693647bb53b1e4a3d460b
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2021-04-01 14:15:36 -0400

    starting work on the morph vs

--- animal3D SDK/resource/glsl/4x/vs/04-anim/passTangentBasis_morph_transform_vs4x.glsl
+++ animal3D SDK/resource/glsl/4x/vs/04-anim/passTangentBasis_morph_transform_vs4x.glsl
@@ -34,11 +34,20 @@
 //		(hint: results can be stored in local variables named after the 
 //		complete tangent basis attributes provided before any changes)
 
+/*
 layout (location = 0) in vec4 aPosition;
 layout (location = 2) in vec3 aNormal;
 layout (location = 8) in vec4 aTexcoord;
 layout (location = 10) in vec3 aTangent;
 layout (location = 11) in vec3 aBitangent;
+*/
+
+// signle morph target: position, normal, tangent
+//  -> we can have 5 targets: 16 total attribs / 3 per target
+//  -> leftover attrib: texcoord
+
+// not morph target: textcoord, bitangent
+
 
 struct sModelMatrixStack
 {
@@ -71,6 +80,13 @@ void main()
 	// DUMMY OUTPUT: directly assign input position to output position
 	//gl_Position = aPosition;
 	
+	vec4 aPosition;
+	vec3 aTangent, aBitangent, aNormal;
+
+	//testing : copy first morph target
+
+
+
 	sModelMatrixStack t = uModelMatrixStack[uIndex];
 	
 	vTangentBasis_view = t.modelViewMatInverseTranspose * mat4(aTangent, 0.0, aBitangent, 0.0, aNormal, 0.0, vec4(0.0));
